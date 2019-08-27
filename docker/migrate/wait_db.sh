ba2faf9d0eec42454660aad77a3bb67f93f03e05
Uro3 <miyakawa.uro3@gmail.com>
2019-08-27 11:05:09 +0900

    first commit

---
+++ docker/migrate/wait_db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+until
+  mysqladmin ping -h ${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD}
+do
+  echo 'waiting for mysql connection...'
+  sleep 3
+done
---
+++ migrations/1_initialize_schema.up.sql
@@ -0,0 +1,5 @@
+create table if not exists users (
+  id integer unsigned not null auto_increment,
+  name varchar(255),
+  primary key (id)
+);
